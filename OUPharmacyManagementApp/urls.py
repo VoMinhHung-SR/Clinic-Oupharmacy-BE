d4554e6c2e27f4259371588bac2e2b455362cb49
VoMinhHung-SR <vominhhung1546@gmail.com>
2025-05-22 16:09:19 +0700

    Refactor: upgrade drf-yasg to 1.20.0 and conditionally include Swagger URLs in urlpatterns based on DEBUG settings.

--- OUPharmacyManagementApp/urls.py
+++ OUPharmacyManagementApp/urls.py
@@ -18,6 +18,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 import debug_toolbar
+import os
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -33,12 +35,15 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('', include('mainApp.urls')),
-
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
 
